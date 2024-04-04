d86cbdc057a237d67faa702d20abd4f5f3934bfb
school-er <144954225+school-er@users.noreply.github.com>
2024-04-04 13:28:11 -0700

    Update baxy.py
    -1 bit cuz

--- baxy.py
+++ baxy.py
@@ -1,5 +1,5 @@
 import time
-def divide(inp, inp2=None): # Baxy Version 1 #
+def divide(inp,inp2=None): # Baxy Version 1 #
   if '/' not in str(inp) and inp2 is None:return "Something went wrong. Error code NOT_VALID_EQUATION"
   if '/' not in str(inp):x=float(inp);y=float(inp2);z=len(str(x))**2;w=0;v=False;u=0;st=time.time();b=0
   else:g=inp.split('/');x=float(g[0]);y=float(g[1]);z=len(str(x))**2;w=0;v=False;u=0;st=time.time();b=0
